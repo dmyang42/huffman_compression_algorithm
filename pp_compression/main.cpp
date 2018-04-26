2225e866fccc708dff10abeed19ceb9b6713058a
topologyYDM <ydm2016@mail.ustc.edu.cn>
2018-04-26 23:46:22 +0800

    Update main.cpp

--- pp_compression/main.cpp
+++ pp_compression/main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include "pp_encode.h"
+#include "pp_decode.h"
 
 int compress_mode()
 {
     std::string FileName;
-    std::cout << "Enter file name : ";
+    std::cout << "Enter file name :";
+    std::cout << ">>";
     std::cin >> FileName;
     pp_encode pp(FileName);
     pp.encode();
     return 0;
 }
 
-int decode_mode()
+int decompress_mode()
 {
+    std::string FileName;
+    std::cout << "Enter file name :";
+    std::cout << ">>";
+    std::cin >> FileName;
+    pp_decode pp(FileName);
+    pp.decode();
     return 0;
 }
 
 int main()
 {
-    compress_mode();
-
+    int mode;
+    std::cout << "Enter mode : (0 for encode, 1 for decode)" << std::endl;
+    std::cout << ">>";
+    std::cin >> mode;
+    if (mode == 0)
+        compress_mode();
+    else if (mode == 1)
+        decompress_mode();
+    else
+        std::cout << "Error!";
     return 0;
 }
